d6efab5be95a884c712336f722b03dc0225a93ec
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-03-18 13:26:41 +0100

    fix nuxt-config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["@swedbankpay/design-guide/src/less/swedbankpay.less"]
+  css: ["@swedbankpay/design-guide/src/less/swedbankpay.less"],
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/scripts'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxt/scripts",
+  ],
+});
