ca00f92ea0d526f7ba7c8444feb718aa0b062736
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-03-18 14:00:51 +0100

    disable SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["@swedbankpay/design-guide/src/less/swedbankpay.less"],
+  ssr: false, // FIXME: Enable SSR. So far DS scripts break it by "window is not defined" when trying to make it SSR. DS should NOT be blocking it, since SSR is best practice where applicable
 
   modules: [
     "@nuxt/content",
